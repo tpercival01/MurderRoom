ae7311994a34596dda51898401de8f8e25913b2b
Thomas Percival <thomaspercival@live.co.uk>
2026-07-21 09:12:25 +0100

    Initial Commit

---
+++ MurderRoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MurderRoom
+//
+//  Created by CAA Guest on 21/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MurderRoomTests/MurderRoomTests.swift
@@ -0,0 +1,19 @@
+//
+//  MurderRoomTests.swift
+//  MurderRoomTests
+//
+//  Created by CAA Guest on 21/07/2026.
+//
+
+import Testing
+@testable import MurderRoom
+
+struct MurderRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
